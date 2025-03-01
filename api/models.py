32f3ed099e145e069d8f56b689dd5a1e208b65d1
Alexhoup27 <houpalexander@yandex.ru>
2025-03-01 21:08:39 +1000

    🤣🤣🤣

--- api/models.py
+++ api/models.py
@@ -1,60 +1,60 @@
 from django.db import models
 
-from graf import model
-
 
 class Pool(models.Model):
-    pool_id = models.AutoField(primary_key=True)
-    pool_name = models.CharField(max_length=100)
-    pool_desc = models.CharField(max_length=255)
-    temperature_sensor_id = models.IntegerField()
-    oxygen_saturation_sensor_id = models.IntegerField()
-    pH_sensor_id = models.IntegerField()
-    orp_sensor_id = models.IntegerField()
-    salinity_sensor_id = models.IntegerField()
-    water_level_sensor_id = models.IntegerField()
-    turbidity_sensor_id = models.IntegerField()
-    ammonia_content_sensor_id = models.IntegerField()
-    nitrite_content_sensor_id = models.IntegerField()
+    pool_id = models.AutoField(primary_key=True, default='0')
+    pool_name = models.CharField(max_length=100, default='Крутой бассейн')
+    pool_desc = models.CharField(max_length=255, default='0 ряд, 0 секция')
+    url = models.URLField(null=True)
+    temperature_sensor_id = models.IntegerField(null=True)
+    oxygen_saturation_sensor_id = models.IntegerField(null=True)
+    pH_sensor_id = models.IntegerField(null=True)
+    orp_sensor_id = models.IntegerField(null=True)
+    salinity_sensor_id = models.IntegerField(null=True)
+    water_level_sensor_id = models.IntegerField(null=True)
+    turbidity_sensor_id = models.IntegerField(null=True)
+    ammonia_content_sensor_id = models.IntegerField(null=True)
+    nitrite_content_sensor_id = models.IntegerField(null=True)
 
     def __str__(self):
         return f"{self.pool_id} (№{self.pool_name}, {self.pool_desc})"
 
 
 class PoolOptimalValues(models.Model):
-    min_temperature = models.FloatField()
-    max_temperature = models.FloatField()
-    min_oxygen_saturation = models.FloatField()
-    max_oxygen_saturation = models.FloatField()
-    min_pH = models.FloatField()
-    max_pH = models.FloatField()
-    min_orp = models.FloatField()
-    max_orp = models.FloatField()
-    min_salinity = models.FloatField()
-    max_salinity = models.FloatField()
-    min_water_level = models.FloatField()
-    max_water_level = models.FloatField()
-    min_turbidity = models.FloatField()
-    max_turbidity = models.FloatField()
-    min_ammonia_content = models.FloatField()
-    max_ammonia_content = models.FloatField()
-    min_nitrite_content = models.FloatField()
-    max_nitrite_content = models.FloatField()
+    pool_id = models.ForeignKey(Pool, on_delete=models.CASCADE, default=0)
+    min_temperature = models.FloatField(null=True)
+    max_temperature = models.FloatField(null=True)
+    min_oxygen_saturation = models.FloatField(null=True)
+    max_oxygen_saturation = models.FloatField(null=True)
+    min_pH = models.FloatField(null=True)
+    max_pH = models.FloatField(null=True)
+    min_orp = models.FloatField(null=True)
+    max_orp = models.FloatField(null=True)
+    min_salinity = models.FloatField(null=True)
+    max_salinity = models.FloatField(null=True)
+    min_water_level = models.FloatField(null=True)
+    max_water_level = models.FloatField(null=True)
+    min_turbidity = models.FloatField(null=True)
+    max_turbidity = models.FloatField(null=True)
+    min_ammonia_content = models.FloatField(null=True)
+    max_ammonia_content = models.FloatField(null=True)
+    min_nitrite_content = models.FloatField(null=True)
+    max_nitrite_content = models.FloatField(null=True)
 
 
 
 class PoolStatistic(models.Model):
-    pool_id = models.ForeignKey(Pool, on_delete=models.CASCADE)
+    pool_id = models.ForeignKey(Pool, on_delete=models.CASCADE, default=0)
     timestamp = models.DateTimeField(auto_now=True)
-    temperature = models.FloatField()
-    oxygen_saturation = models.FloatField()
-    pH = models.FloatField()
-    orp = model.FloatField()
-    salinity = models.FloatField()
-    water_level = models.FloatField()
-    turbidity = models.FloatField()
-    ammonia_content = models.FloatField()
-    nitrite_content = models.FloatField()
+    temperature = models.FloatField(null=True)
+    oxygen_saturation = models.FloatField(null=True)
+    pH = models.FloatField(null=True)
+    orp = models.FloatField(null=True)
+    salinity = models.FloatField(null=True)
+    water_level = models.FloatField(null=True)
+    turbidity = models.FloatField(null=True)
+    ammonia_content = models.FloatField(null=True)
+    nitrite_content = models.FloatField(null=True)
 
     def __str__(self):
         return f"{self.pool_id})"
--- api/services/helpers.py
+++ api/services/helpers.py
@@ -1,8 +1,57 @@
 from ..models import Pool, PoolOptimalValues, PoolStatistic
 
 
-def get_pools():
-    return Pool.objects.all()
+class PoolManagment:
+    def __init__(self):
+        self.data = Pool.objects
+    def take_data(self, pool_id):
+        if self.data.filter(pool_id=pool_id).exists():
+            to_return = self.data.filter(pool_id=pool_id)
+            return to_return
+        else:
+            print(f'pool {pool_id} does`t exists')
+            return []
+    def write_data(self, pool_id, pool_name, pool_desc, url,
+                   temperature_sensor_id, oxygen_saturation_sensor_id,
+                   pH_sensor_id, orp_sensor_id, salinity_sensor_id,
+                   water_level_sensor_id, turbidity_sensor_id,
+                   ammonia_content_sensor_id, nitrite_content_sensor_id):
+        if self.data.filter(pool_id=pool_id).exists():
+            now_data = self.data.filter(pool_id=pool_id)
+            now_data.pool_name = pool_name
+            now_data.pool_desc = pool_desc
+            now_data.url = url
+            now_data.temperature_sensor_id = temperature_sensor_id
+            now_data.oxygen_saturation_sensor_id = oxygen_saturation_sensor_id
+            now_data.pH_sensor_id = pH_sensor_id
+            now_data.orp_sensor_id = orp_sensor_id
+            now_data.salinity_sensor_id = salinity_sensor_id
+            now_data.water_level_sensor_id = water_level_sensor_id
+            now_data.turbidity_sensor_id = turbidity_sensor_id
+            now_data.ammonia_content_sensor_id = ammonia_content_sensor_id
+            now_data.nitrite_content_sensor_id = nitrite_content_sensor_id
+            now_data.save()
+            print(f'pool info {pool_id} successfully rewrite')
+            return True
+        else:
+            new_pool = Pool(
+                pool_id=pool_id,
+                pool_name=pool_name,
+                pool_desc=pool_desc,
+                url=url,
+                temperature_sensor_id=temperature_sensor_id,
+                oxygen_saturation_sensor_id=oxygen_saturation_sensor_id,
+                pH_sensor_id=pH_sensor_id,
+                orp_sensor_id=orp_sensor_id,
+                salinity_sensor_id=salinity_sensor_id,
+                water_level_sensor_id=water_level_sensor_id,
+                turbidity_sensor_id=turbidity_sensor_id,
+                ammonia_content_sensor_id=ammonia_content_sensor_id,
+                nitrite_content_sensor_id =nitrite_content_sensor_id
+            )
+            new_pool.save()
+            print(f'pool {pool_id} successful create')
+            return True
 class Pool_Values_Managment:
     def __init__(self):
         self.data = PoolOptimalValues.objects
@@ -11,7 +60,7 @@ class Pool_Values_Managment:
             to_return = self.data.filter(pool_id=pool_id)
             return to_return
         else:
-            print(f'pool {pool_id} don`t exists')
+            print(f'pool {pool_id} does`t exists ')
             return []
     def write_data(self, pool_id, timestamp, temperature,
                    oxygen_saturation, pH, orp, salinity, water_level,
@@ -45,5 +94,36 @@ class Pool_Values_Managment:
                 ammonia_content=ammonia_content,
                 nitrite_content=nitrite_content
             )
+            new_data.save()
             print(f'pool data {pool_id} successfully create')
             return True
+class Pool_Statistic_Managment:
+    def __init__(self):
+        self.data = PoolStatistic.objects
+    def take_data(self, pool_id):
+        if self.data.filter(pool_id=pool_id).exists():
+            to_return = self.data.filter(pool_id=pool_id)
+            return to_return
+        else:
+            print(f'pool {pool_id} does`t exists ')
+            return []
+    def write_data(self, pool_id, timestamp, temperature,
+                   oxygen_saturation, pH, orp, salinity,
+                   water_level, turbidity, ammonia_content,
+                   nitrite_content):
+        new_data = PoolStatistic(
+            pool_id=pool_id,
+            timestamp=timestamp,
+            temperature=temperature,
+            oxygen_saturation=oxygen_saturation,
+            pH=pH,
+            orp=orp,
+            salinity=salinity,
+            water_level=water_level,
+            turbidity=turbidity,
+            ammonia_content=ammonia_content,
+            nitrite_content=nitrite_content
+        )
+        new_data.save()
+        print(f'pool {pool_id} successful create')
+        return 
