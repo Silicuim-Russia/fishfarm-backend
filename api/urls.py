fc3b6a8c780a4b7315d36c987223277f615116f6
Alexhoup27 <houpalexander@yandex.ru>
2025-03-02 01:56:46 +1000

    💩

--- api/urls.py
+++ api/urls.py
@@ -9,6 +9,8 @@ from .views import *
 urlpatterns = [
     path('login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-    # path('all-pools/', AllPools.as_view(), name='all-pools'),
+    path('all-pools/', AllPools.as_view(), name='all-pools'),
     path('status/', get_status, name='status'),
+    # path('update/', update_parameters, name='update'),
+    path('update/', AllPools.as_view(), name='update')
 ]
